0bddcfa39040120a23f8fcf81597bf25160de21c
olebas13 <o.nevoyt13@gmail.com>
2018-08-21 18:07:18 +0300

    finish lesson 58

---
+++ p0581timepickerdialog/src/main/java/com/olebas/p0581timepickerdialog/MainActivity.java
@@ -0,0 +1,48 @@
+package com.olebas.p0581timepickerdialog;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import static android.app.TimePickerDialog.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    int DIALOG_TIME = 1;
+    int myHour = 14;
+    int myMinute = 35;
+    TextView tvTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+    }
+
+    public void onClick(View view) {
+        showDialog(DIALOG_TIME);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    OnTimeSetListener myCallBack = new OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            tvTime.setText("Time is " + myHour + " hours " + myMinute + " minutes");
+        }
+    };
+}
