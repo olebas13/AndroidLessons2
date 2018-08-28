a71d2440294455a9db3db6720ed6c59b1e92c468
olebas13 <o.nevoyt13@gmail.com>
2018-08-28 17:48:01 +0300

    finish lesson 67

---
+++ p0671progressdialog/src/main/java/com/olebas/p0671progressdialog/MainActivity.java
@@ -0,0 +1,65 @@
+package com.olebas.p0671progressdialog;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import static android.content.DialogInterface.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog pd;
+    Handler h;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnDefault:
+                pd = new ProgressDialog(this);
+                pd.setTitle("Title");
+                pd.setMessage("Message");
+                pd.setButton(Dialog.BUTTON_POSITIVE, "OK", new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                pd.show();
+                break;
+            case R.id.btnHoriz:
+                pd = new ProgressDialog(this);
+                pd.setTitle("Title");
+                pd.setMessage("Message");
+                pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pd.setMax(2148);
+                pd.setIndeterminate(true);
+                pd.show();
+                h = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        pd.setIndeterminate(false);
+                        if (pd.getProgress() < pd.getMax()) {
+                            pd.incrementProgressBy(50);
+                            pd.incrementSecondaryProgressBy(75);
+                            h.sendEmptyMessageDelayed(0, 100);
+                        } else {
+                            pd.dismiss();
+                        }
+                    }
+                };
+                h.sendEmptyMessageDelayed(0, 2000);
+                break;
+            default:
+                break;
+        }
+    }
+}
